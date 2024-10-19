e6e56c4703f3832cab45ff1b0604be66ecc0edf8
Jorn3355 <RealJornShyti@gmail.com>
2024-10-19 14:28:12 +0200

    Improved book cards and added a remove button on each one, midway to adding a popup form for each new card.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const addBtn = document.querySelector("#add-book")
 const bookContainer = document.querySelector(".book-container")
+const body = document.querySelector("body")
 
 const bookArray = [];
 
@@ -9,20 +10,44 @@ function Book(title="missing", author="missing", read=false) {
     this.Read = read
 }
 
+
+function popupForm() {
+    
+}
+
 function addBook() {
-    let newBook = new Book("Fujimoto", "Chainsaw-Man", true)
+    let newBook = new Book("Chain-Saw Man", "Fujimoto", true)
     let newCard = document.createElement("div")
     newCard.classList.add("book-Card")
     for(let prop in newBook){
-        if(Object.prototype.hasOwnProperty.call(newBook, prop)){   
-            let newItem = document.createElement("span")
-            newItem.textContent = `${prop}: ${newBook[prop]}`
+        if(Object.prototype.hasOwnProperty.call(newBook, prop)){
+            let newItem 
+            if(prop === "Read") {
+                newItem = document.createElement("div")
+                newItem.classList.add("read")
+                newItem.textContent = "Read"
+            } else {
+                newItem = document.createElement("span")
+                newItem.textContent = `${prop}: ${newBook[prop]}`
+            }
             newCard.appendChild(newItem)
-        } else {
-            
         }
+    bookArray.push[newBook]
     bookContainer.appendChild(newCard)
-}}
+}
+    let rmBtn = document.createElement("button")
+    rmBtn.textContent = "Remove"
+    rmBtn.classList.add("rm-btn")
+    newCard.appendChild(rmBtn)
+}
 
-addBtn.addEventListener("click", addBook)
+addBtn.addEventListener("click", () => {
+    addBook();
+    let rmBtns = document.querySelectorAll(".rm-btn")
+    rmBtns.forEach((item) => {
+        item.addEventListener("click", (e) => {e.target.parentElement.remove()})
+    })
+})
+
+
 
